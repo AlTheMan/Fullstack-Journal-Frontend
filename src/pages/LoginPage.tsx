3654d83c09be5f4bff34c4b3ff30d871569fc798
AlTheMan <algot98@hotmail.com>
2023-11-13 17:26:06 +0100

    axios

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,45 +2,23 @@
 import React from 'react';
 import Button from '../components/Button';
 import LoginForm from '../components/LoginForm';
+import axios from 'axios';
+
 
 const LoginPage = () => {
   const handleLoginFormSubmit = async (username: string, password: string) => {
-    // Handle the form submission logic here
     console.log('Submitted from LoginPage:', username, password);
 
-    try {
-      // Define the data to be sent in the request body
-      const requestData = {
-        username: username,
-        password: password,
-      };
+    // Define the data to be sent in the request body
+    const requestData = {
+      username: username,
+      password: password,
+    };
 
-      // Make the HTTP POST request using fetch
-      const response = await fetch('http://localhost:8080/user/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(requestData),
-      });
-
-      //alt:
-       // Make the HTTP POST request using Axios
-       //const response = await axios.post('http://localhost:8080/user/login', requestData);
-
-
-      // Check if the request was successful
-      if (response.ok) {
-        // Parse the response JSON if needed
-        const responseData = await response.json();
-        console.log('Response from server:', responseData);
-      } else {
-        // Handle errors
-        console.error('Error:', response.statusText);
-      }
-    } catch (error) {
-      console.error('Error:');
-    }
+      // Make the HTTP POST request using Axios
+      const response = await axios.post('http://localhost:8080/user/login', requestData);
+      console.log(response.status);
+      console.log(response.data);
 
   };
 
