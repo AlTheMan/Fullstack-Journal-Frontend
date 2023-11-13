5fa2c4e5f3cf5dd5def3eb54d0fc5064bb8b422d
AlTheMan <algot98@hotmail.com>
2023-11-13 13:42:48 +0100

    button added

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
+interface Props{
+    children: string;
+    onClick: ()=>void;
+    color?: 'primary' | 'secondary' | 'danger';
+}
 
-
-const Button = () =>{
+const Button = ({children, onClick, color ='primary'}: Props) =>{
     return (
-        <button> Button</button>
+        <button className={'btn btn-' + color} onClick={onClick}> {children}</button>
     )
 }
 
