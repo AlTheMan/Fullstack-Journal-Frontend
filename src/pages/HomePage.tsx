d52b284853ce71f756fa6ae966ca0d9567beca85
AlTheMan <algot98@hotmail.com>
2023-11-13 13:52:54 +0100

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,10 @@ import Button from "../components/Button";
 
 const HomePage = () =>{
     return (
-        <div><Button onClick={()=>console.log('clicked')}> login</Button></div>
+        <div>
+            <Button onClick={()=>console.log('clicked')}> login</Button>
+            <Button onClick={()=>console.log('clicked')}> Register</Button>
+        </div>
     )
 }
 
