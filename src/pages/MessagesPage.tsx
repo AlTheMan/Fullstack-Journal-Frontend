d068cb90d5f88513e630826455bf67c12d37bf87
Emil Wennström <emilwennstrom@gmail.com>
2023-11-15 19:44:43 +0100

    Fixade så att chattboxen är fixerad samt att sidan skrollar vid meddelande

--- src/pages/MessagesPage.tsx
+++ src/pages/MessagesPage.tsx
@@ -1,5 +1,5 @@
 // MessagesPage.tsx
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 import Message from '../components/Message';
 import NavBar from '../components/Navbar';
@@ -44,6 +44,25 @@ const MessagesPage = () => {
     const [patients, setPatients] = useState<Patient[]>([]);
     const [messages, setMessages] = useState<Message[]>([]);
     const [myId, setMyId] = useState<MyIdType | undefined>(undefined);
+    const messagesEndRef = useRef<null | HTMLDivElement>(null);
+
+    const scrollToBottom = () => {
+        messagesEndRef.current?.scrollIntoView({ behavior: "smooth"});
+    };
+
+    useEffect(() => {
+        if (messages.length > 0) {
+            // Use a timeout to allow the browser to paint the new message
+            // This is a common trick to deal with timing issues in React
+            const timer = setTimeout(() => {
+                scrollToBottom();
+            }, 100); // Adjust the timeout if necessary
+    
+            // Clean up the timeout when the effect is re-run or on component unmount
+            return () => clearTimeout(timer);
+        }
+    }, [messages]);
+    
 
 
     useEffect(() => {
@@ -165,55 +184,63 @@ const MessagesPage = () => {
 
   return (
     <>
-    <NavBar></NavBar>
-    <div>{/* Render the list of doctors/staff or people */}
+  <NavBar />
+  <div style={{ paddingBottom: '100px', backgroundColor: 'transparent'}}> {/* Add padding to bottom equal to the height of the fixed form */}
     <h2>List of People:</h2>
-        <ul>
-            {/* Visar patienter här */}
-            {localStorage.getItem('privilege') === 'PATIENT' ? (
-                //Visar doctor/staff lista
-                <ListGroupGeneric<Doctor>
-                items={doctors}
-                getKey={(doctor) => doctor.id.toString()}
-                getLabel={(doctor) => `${doctor.firstName} ${doctor.lastName} (ID: ${doctor.id}) ${doctor.privilege}`}
-                onSelectItem={(doctor) => handleSelectPerson(doctor.id)}
-                />
-            
-          ) : (
-                //Visar patientlista
-                <ListGroupGeneric<Patient>
-                items={patients}
-                getKey={(patient) => patient.patientId.toString()}
-                getLabel={(patient) => `${patient.firstName} ${patient.familyName} (ID: ${patient.patientId})`}
-                onSelectItem={(patient) => handleSelectPerson(patient.patientId)}
-                />
-          )}
-        </ul>
-        <ul>
-            {/* Visar meddelanden här */}
-            {messages.map((message) => (
-                <li key={message.id}
-                style={{
-                    listStyleType: 'none',
-                    padding: '12px',
-                    marginRight: '32px',
-                    textAlign: message.sentById == myId?.id ? 'right' : 'left',
-                    backgroundColor: message.sentById == myId?.id ? '#cce5ff' : '#f0f0f0',
-                }}
-                >
-                    {message.message} <br />
-                </li>
-            ))}
-
-            {/* Skickar meddelanden här */}
-            <div>
-                <MessageForm onSubmit={handleSendMessage} />
-            </div>
+    <div style={{ overflowY: 'auto', height: 'calc(100vh - 100px)' }}> {/* Make this div scrollable */}
+      {/* Render the list of doctors/staff or people */}
+      <ul>
+        {localStorage.getItem('privilege') === 'PATIENT' ? (
+          //Visar doctor/staff lista
+          <ListGroupGeneric<Doctor>
+            items={doctors}
+            getKey={(doctor) => doctor.id.toString()}
+            getLabel={(doctor) => `${doctor.firstName} ${doctor.lastName} (ID: ${doctor.id}) ${doctor.privilege}`}
+            onSelectItem={(doctor) => handleSelectPerson(doctor.id)}
+          />
+        ) : (
+          //Visar patientlista
+          <ListGroupGeneric<Patient>
+            items={patients}
+            getKey={(patient) => patient.patientId.toString()}
+            getLabel={(patient) => `${patient.firstName} ${patient.familyName} (ID: ${patient.patientId})`}
+            onSelectItem={(patient) => handleSelectPerson(patient.patientId)}
+          />
+        )}
       </ul>
       <ul>
+        {/* Render messages here */}
+        {messages.map((message) => (
+          <li key={message.id} style={{
+            listStyleType: 'none',
+            padding: '12px',
+            marginRight: '32px',
+            textAlign: message.sentById == myId?.id ? 'right' : 'left',
+            backgroundColor: message.sentById == myId?.id ? '#cce5ff' : '#f0f0f0',
+          }}>
+            {message.message} <br />
+          </li>
+        ))}
       </ul>
+      <div ref={messagesEndRef}></div>
     </div>
-    </>
+  </div>
+  
+  {/* Message form with fixed position */}
+  <div style={{
+    position: 'fixed',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    width: '100%',
+    padding: '12px',
+    backgroundColor: 'transparent'
+  }}>
+    <MessageForm onSubmit={handleSendMessage} />
+  </div>
+  
+</>
+
   );
 };
 
