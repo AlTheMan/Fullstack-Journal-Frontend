6093a84fd4736b5f054709a766b7fed439668ec2
Emil Wennström <emilwennstrom@gmail.com>
2023-11-19 18:10:36 +0100

    Update DoctorHome.tsx

--- src/pages/DoctorHome.tsx
+++ src/pages/DoctorHome.tsx
@@ -1,11 +1,10 @@
 import { fetchData } from "../api/namedPersonApi";
-import React, { useEffect, useState } from 'react';
-import Button from '../components/Button';
-import { Link, useNavigate } from 'react-router-dom';
-import ListGroupGeneric from '../components/ListGroupGeneric';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import Button from "../components/Button";
+import { Link, useNavigate } from "react-router-dom";
+import ListGroupGeneric from "../components/ListGroupGeneric";
+import axios from "axios";
 import { Patient } from "../types/Patient";
-
 
 const DoctorHome: React.FC = () => {
   const [doctor, setDoctor] = useState<NamedPerson | null>(null);
@@ -26,66 +25,75 @@ const DoctorHome: React.FC = () => {
   var patientId = 11;
 
   const [patients, setPatients] = useState<Patient[]>([]);
-  const [selectedPatientId, setSelectedPatientId] = useState<number | null>(null);
+  const [selectedPatientId, setSelectedPatientId] = useState<number | null>(
+    null
+  );
 
   const navigate = useNavigate();
 
-    const handleSelectPerson=(patientId:number ) =>{
-        const myId: number = Number(localStorage.getItem("id")) || -1; //ifall att numret är null så sätts värdet till "-1"
-        console.log("my id: " + myId)
-        const privilege: string = localStorage.getItem("privilege") || "";
-        setSelectedPatientId(patientId);
-        console.log("patient id: " + patientId)
-        navigate('/DoctorSelect', { state: { patientId } });
+  const handleSelectPerson = (patientId: number) => {
+    const myId: number = Number(localStorage.getItem("id")) || -1; //ifall att numret är null så sätts värdet till "-1"
+    console.log("my id: " + myId);
+    const privilege: string = localStorage.getItem("privilege") || "";
+    setSelectedPatientId(patientId);
+    console.log("patient id: " + patientId);
+    navigate("/DoctorSelect", { state: { patientId } });
+  };
 
+  useEffect(() => {
+    const storedPatients = localStorage.getItem("patients");
+
+    if (storedPatients) {
+      // If patients are in local storage, parse and set to state
+      const patientData: Patient[] = JSON.parse(storedPatients);
+      setPatients(patientData);
     }
+    // If not in local storage, fetch from the server
 
-
-    useEffect(() => {
-      const storedPatients = localStorage.getItem('patients');
-
-      if (storedPatients) {
-          // If patients are in local storage, parse and set to state
-          const patientData: Patient[] = JSON.parse(storedPatients);
-          setPatients(patientData);
-      } else {
-          // If not in local storage, fetch from the server
-          const fetchDataPatients = async () => {
-              const response = await axios.get('http://localhost:8080/patient/get_all');
-              if (response.status === 200) {
-                  const patientData: Patient[] = response.data;
-                  setPatients(patientData);
-                  localStorage.setItem('patients', JSON.stringify(patientData));
-              }
-          };
-          fetchDataPatients();
+    const fetchDataPatients = async () => {
+      const response = await axios.get("http://localhost:8080/patient/get_all");
+      if (response.status === 200) {
+        const patientData: Patient[] = response.data;
+        setPatients(patientData);
+        localStorage.setItem("patients", JSON.stringify(patientData));
       }
-  }, []); // Empty dependency array to run the effect only once
-    
+    };
 
-return (
+    fetchDataPatients();
+  }, []); // Empty dependency array to run the effect only once
+
+  return (
     <div>
       <h1>
         Welcome: Dr {doctor?.firstName} {doctor?.lastName}
       </h1>
-      <Link to={selectedPatientId !== null ? `/NotePage/${selectedPatientId}` : '#'} className="nav-link">
-          <Button onClick={() => console.log(`Patient id: ${selectedPatientId}`)}>
-              Add note
-          </Button>
+      <Link
+        to={selectedPatientId !== null ? `/NotePage/${selectedPatientId}` : "#"}
+        className="nav-link"
+      >
+        <Button onClick={() => console.log(`Patient id: ${selectedPatientId}`)}>
+          Add note
+        </Button>
       </Link>
-       <div style={{ paddingBottom: '100px', backgroundColor: 'transparent'}}> {/* Add padding to bottom equal to the height of the fixed form */}
+      <div style={{ paddingBottom: "100px", backgroundColor: "transparent" }}>
+        {" "}
+        {/* Add padding to bottom equal to the height of the fixed form */}
         <h2>List of Patients:</h2>
-        <div style={{ overflowY: 'auto', height: 'calc(100vh - 100px)' }}> {/* Make this div scrollable */}
-            <ul>
-                <ListGroupGeneric<Patient>
-                    items={patients}
-                    getKey={(patient) => patient.patientId.toString()}
-                    getLabel={(patient) => `${patient.firstName} ${patient.familyName} (ID: ${patient.patientId})`}
-                    onSelectItem={(patient) => handleSelectPerson(patient.patientId)}
-                />
-            </ul>
+        <div style={{ overflowY: "auto", height: "calc(100vh - 100px)" }}>
+          {" "}
+          {/* Make this div scrollable */}
+          <ul>
+            <ListGroupGeneric<Patient>
+              items={patients}
+              getKey={(patient) => patient.patientId.toString()}
+              getLabel={(patient) =>
+                `${patient.firstName} ${patient.familyName} (ID: ${patient.patientId})`
+              }
+              onSelectItem={(patient) => handleSelectPerson(patient.patientId)}
+            />
+          </ul>
         </div>
-    </div>
+      </div>
     </div>
   );
 };
