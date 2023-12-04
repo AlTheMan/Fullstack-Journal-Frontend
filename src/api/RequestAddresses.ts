8f1b83a0deb115d0d709e185f6be7ff921b85e6b
Emil Wennström <emilwennstrom@gmail.com>
2023-12-04 18:27:20 +0100

    Update RequestAddresses.ts

--- src/api/RequestAddresses.ts
+++ src/api/RequestAddresses.ts
@@ -1,9 +1,9 @@
 
 export const patientApiAddress = () => {
     // 8081
-    const port = 8081;
-    const address = 'http://localhost:'+ port +'/patient';
-
+    //const port = 8081;
+    //const address = 'http://localhost:'+ port +'/patient';
+    const address = 'https://patient-api.app.cloud.cbh.kth.se/' + 'patient'
     return address;
 
 }
@@ -11,15 +11,19 @@ export const patientApiAddress = () => {
 
 export const userApiAddress = () => {
     // 8083
-    const port = 8083;
-    const address = 'http://localhost:'+ port +'/user';
+    //const port = 8083;
+    //const address = 'http://localhost:'+ port +'/user';
+
+    const address = 'https://user-api.app.cloud.cbh.kth.se/' + 'user'
+
     return address;
 }
 
 export const messageApiAddress = () => {
 
     //8082
-    const port = 8082;
-    const address = 'http://localhost:'+ port +'/messages';
+    //const port = 8082;
+    //const address = 'http://localhost:'+ port +'/messages';
+    const address = 'https://message-api.app.cloud.cbh.kth.se/' + 'messages'
     return address;
 }
