3099a67681cb7f9e87df8c2843234e76d652df6d
Emil Wennström <emilwennstrom@gmail.com>
2023-12-07 21:43:48 +0100

    funkar typ

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,51 +1,189 @@
-import { useEffect } from "react";
-import "../App.css";
+import React, { useEffect, useRef, useState } from "react";
+import "../pages/images/Image.css";
 
 interface CanvasComponentProps {
   imageUrl: string;
-  canvasRef: React.RefObject<HTMLCanvasElement>
+  addText: boolean;
+  hexColor: string
 }
 
-const CanvasComponent: React.FC<CanvasComponentProps> = ({ imageUrl, canvasRef }) => {
+const CanvasComponent: React.FC<CanvasComponentProps> = ({
+  imageUrl,
+  addText,
+  hexColor
+}) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const contextRef = useRef<CanvasRenderingContext2D | null>(null);
+  const textInputRef = useRef<HTMLInputElement | null>(null);
+  const isTextShown = useRef(false);
+  const isDrawingRef = useRef(false);
+  const lastPointRef = useRef<{ x: number; y: number }>({ x: 0, y: 0 });
+  const [loaded, setLoaded] = useState(false);
+
+
 
   useEffect(() => {
-    if (canvasRef.current) {
-      const canvas = canvasRef.current;
-      const context = canvas.getContext("2d");
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+    const context = canvas.getContext("2d");
+    if (!context) return;
 
-      if (context) {
-        // Load the image
-        const image = new Image();
-        image.src = imageUrl;
-        canvas.className = "image-size";
-        image.onload = () => {
-        
-          let scale = Math.min(
-            1280 / image.naturalWidth,
-            720 / image.naturalHeight
-          );
-          scale = scale > 1 ? 1 : scale; // Ensure we don't scale up the image
+    contextRef.current = context;
 
-          canvas.width = image.naturalWidth * scale
-          canvas.height = image.naturalHeight * scale
+    const image = new Image();
+    image.src = imageUrl;
+    image.onload = () => {
+      const scale = Math.min(
+        800 / image.naturalWidth,
+        800 / image.naturalHeight
+      );
 
-          // Draw the image
-          context.drawImage(image, 0, 0, canvas.width, canvas.height);
+      const scaledWidth = image.naturalWidth * scale;
+      const scaledHeight = image.naturalHeight * scale;
 
-          // Now you can draw on the canvas here
-          // Example: Draw a line
-          context.beginPath();
-          context.moveTo(50, 50);
-          context.lineTo(200, 400);
-          context.stroke();
-        };
-      }
-    }
+      canvas.style.width = `${scaledWidth}px`;
+      canvas.style.height = `${scaledHeight}px`;
+
+      canvas.width = image.naturalWidth;
+      canvas.height = image.naturalHeight;
+      context.drawImage(image, 0, 0, image.naturalWidth, image.naturalHeight);
+      setLoaded(true); // Indicates that the image is loaded and drawn
+    };
   }, [imageUrl]);
+
+
+  useEffect(() => {
+
+    if (!loaded) return;
+
+    const context = contextRef.current;
+    if (!context) return;
+    const canvas = canvasRef.current
+    if (!canvas) return;
+
+    context.strokeStyle = hexColor;
+    context.fillStyle = hexColor;
+
+    
+    
+
+
+    const getMousePosition = (canvas: HTMLCanvasElement, event: MouseEvent) => {
+      const rect = canvas.getBoundingClientRect();
+      const scaleX = canvas.width / rect.width; // relationship bitmap vs. element for X
+      const scaleY = canvas.height / rect.height; // relationship bitmap vs. element for Y
+
+      return {
+        x: (event.clientX - rect.left) * scaleX, // scale mouse coordinates after they have
+        y: (event.clientY - rect.top) * scaleY, // been adjusted to be relative to element
+      };
+    };
+    if (addText) {
+      const drawLine = (x1: number, y1: number, x2: number, y2: number) => {
+        context.beginPath();
+        context.lineWidth = 10; // Change as needed
+        context.moveTo(x1, y1);
+        context.lineTo(x2, y2);
+        context.stroke();
+        context.closePath();
+      };
+
+      const handleMouseDown = (event: MouseEvent) => {
+        const mousePos = getMousePosition(canvas, event);
+        isDrawingRef.current = true;
+        lastPointRef.current = mousePos;
+      };
+
+      const handleMouseMove = (event: MouseEvent) => {
+        if (!isDrawingRef.current) return;
+        const mousePos = getMousePosition(canvas, event);
+        drawLine(
+          lastPointRef.current.x,
+          lastPointRef.current.y,
+          mousePos.x,
+          mousePos.y
+        );
+        lastPointRef.current = mousePos;
+      };
+
+      const handleMouseUp = () => {
+        isDrawingRef.current = false;
+      };
+
+      canvas.addEventListener("mousedown", handleMouseDown);
+      canvas.addEventListener("mousemove", handleMouseMove);
+      window.addEventListener("mouseup", handleMouseUp);
+
+      return () => {
+        canvas.removeEventListener("mousedown", handleMouseDown);
+        canvas.removeEventListener("mousemove", handleMouseMove);
+        window.removeEventListener("mouseup", handleMouseUp);
+      };
+    } else {
+
+      const showTextField = (event: MouseEvent) => {
+        if (!isTextShown.current) {
+          const relMousePos = getMousePosition(canvas, event)
+          textInputRef.current = document.createElement("input");
+
+          textInputRef.current.type = "text";
+          textInputRef.current.style.position = "absolute";
+          textInputRef.current.style.left = `${event.clientX}px`;
+          textInputRef.current.style.top = `${event.clientY}px`;
+          lastPointRef.current.x = relMousePos.x
+          lastPointRef.current.y = relMousePos.y
+
+          console.log(lastPointRef.current.x);
+          console.log(lastPointRef.current.y);
+
+          document.body.appendChild(textInputRef.current);
+
+          textInputRef.current.focus();
+          isTextShown.current = true;
+        } else {
+          if (textInputRef.current) {
+            const fontSize = (canvas.height/100 * canvas.width/100) / 10
+            const fontFamily = "Arial"; // Set the font family as needed
+            
+            const text = textInputRef.current.value;
+            
+            console.log(text);
+            console.log(lastPointRef.current.x);
+            console.log(lastPointRef.current.y);
+            context.textAlign = "left";
+            context.textBaseline = "top";
+
+            context.fillStyle = hexColor;
+            context.font = `${fontSize}px ${fontFamily}`;
+            context.fillText(
+              text,
+              lastPointRef.current.x,
+              lastPointRef.current.y
+            );
+            if (document.body.contains(textInputRef.current)){
+              document.body.removeChild(textInputRef.current);
+            }
+              
+            textInputRef.current = null;
+          }
+          isTextShown.current = false;
+        }
+      };
+
+      canvas.addEventListener("click", showTextField);
+
+      return () => {
+        canvas.removeEventListener("click", showTextField);
+        if (textInputRef.current) {
+          document.body.removeChild(textInputRef.current);
+        }
+      };
+    }
+  }, [hexColor]);
 
   return (
     <div>
-      <canvas ref={canvasRef} className="image-size"></canvas>
+      <canvas ref={canvasRef} className="image-size" />
     </div>
   );
 };
--- src/pages/images/ImagePage.tsx
+++ src/pages/images/ImagePage.tsx
@@ -1,142 +1,190 @@
 import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
-import React, {useEffect, useState, useRef} from "react";
-import {fetchImage, fetchImageMetadata} from "./ImageApi";
+import React, { useEffect, useState, useRef } from "react";
+import { fetchImage, fetchImageMetadata } from "./ImageApi";
 import LoadingSpinner from "../../components/LoadingSpinner";
 import NavBarDoctor from "../../components/NavBarDoctor";
-import "../../App.css";
 import "./Image.css";
 import CanvasComponent from "../../components/Canvas";
-import {Button} from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import { Color, ColorResult, GithubPicker } from "react-color";
 
 const ImagesPage: React.FC = () => {
-    const [selectedPatient, setSelectedPatient] = useState<Patient | null>(null);
-    const [imagesMetaData, setImagesMetaData] = useState<ImageMetadata | null>(null);
-    const [loading, setLoading] = useState<boolean>(false);
-    const [imageSrc, setImageSrc] = useState<string>("");
-    const canvasRef = useRef(null);
-    const [displayImage, setDisplayImage] = useState<boolean>(false);
-    const fileInputRef = useRef<HTMLInputElement>(null);
+  const [selectedPatient, setSelectedPatient] = useState<Patient | null>(null);
+  const [imagesMetaData, setImagesMetaData] = useState<ImageMetadata | null>(
+    null
+  );
+  const [loading, setLoading] = useState<boolean>(false);
+  const [imageSrc, setImageSrc] = useState<string>("");
+  const [showImage, setShowImage] = useState<boolean>(false);
+  const [showSaveButton, setShowSaveButton] = useState<boolean>(false);
+  const [isDoctor, setIsDoctor] = useState<boolean>(false);
+  const [canvasColor, setCanvasColor] = useState("");
 
-    const handleFileInputChange = async (event) => {
-        const file = event.target.files[0];
-        if (file) {
-            // Check if the file is an image
-            if (file.type === 'image/jpeg' || file.type === 'image/png') {
-                // Implement the logic to upload the file to your API
-                const formData = new FormData();
-                formData.append('image', file);
+  const fileInputRef = useRef<HTMLInputElement>(null);
 
-                try {
-                    const response = await fetch('YOUR_API_ENDPOINT', {
-                        method: 'POST', body: formData, // Add any additional headers if needed
-                    });
-                    const result = await response.json();
-                    console.log(result);
-                    // Handle the API response here
-                } catch (error) {
-                    console.error('Error uploading the file:', error);
-                    // Handle any errors here
-                }
-            } else {
-                console.error('File is not an image.');
-                // Handle the case where the file is not an image
-            }
+  const handleFileInputChange = async (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      // Check if the file is an image
+      if (file.type === "image/jpeg" || file.type === "image/png") {
+        // Implement the logic to upload the file to your API
+        setShowImage(true);
+        const imageUrl = URL.createObjectURL(file);
+        console.log(imageUrl);
+        setImageSrc(URL.createObjectURL(file));
+        setShowSaveButton(true);
+      } else {
+        console.error("File is not an image.");
+      }
+    }
+  };
+
+  const openFileSelector = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const handleSaveImage = () => {
+    setShowSaveButton(false);
+  };
+
+  const handleEditImage = () => {
+    setShowSaveButton(true);
+  };
+
+  const handleDropdownItemClick = async (itemId: string) => {
+    if (imagesMetaData && imagesMetaData.mongoId) {
+      try {
+        const imageData = await fetchImage(itemId, imagesMetaData.mongoId);
+        if (imageData) {
+          const base64Image = imageData.base64Image;
+          const contentType = imageData.contentType;
+          const fullImageSrc = "data:" + contentType + ";base64," + base64Image;
+          setImageSrc(fullImageSrc);
+          setShowImage(true);
+          setShowSaveButton(false);
         }
-    };
+      } catch (error) {
+        console.error("An error occurred: ", error);
+      }
+    }
+  };
 
-    const openFileSelector = () => {
-        if (fileInputRef.current) {
-            fileInputRef.current.click();
+  const handleChangeComplete = (color: ColorResult) => {
+        console.log(color.hex)
+        setCanvasColor(color.hex)
+  };
+
+  useEffect(() => {
+    const storedPatient = localStorage.getItem("currentPatient");
+    const privilege = localStorage.getItem("privilege");
+    if (storedPatient) {
+      const patientData: Patient = JSON.parse(storedPatient);
+      setSelectedPatient(patientData);
+    }
+    if (privilege == "DOCTOR") {
+      setIsDoctor(true);
+    } else {
+      setIsDoctor(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (selectedPatient) {
+      const loadImages = async () => {
+        setLoading(true);
+        try {
+          const metadata = await fetchImageMetadata(selectedPatient.id);
+          if (metadata) {
+            console.log("Metadata fetched");
+            setImagesMetaData(metadata);
+          } else {
+            console.log("Couldn't get metadata");
+          }
+        } catch (error) {
+          console.error("Error", error);
+        } finally {
+          setLoading(false);
         }
-    };
+      };
+      loadImages().then((r) => r);
+    }
+  }, [selectedPatient]);
 
+  const imageMeta = imagesMetaData?.images ?? [];
 
-    const handleDropdownItemClick = async (itemId: string) => {
-        if (imagesMetaData && imagesMetaData.mongoId) {
-            try {
-                const imageData = await fetchImage(itemId, imagesMetaData.mongoId);
-                if (imageData) {
-                    const base64Image = imageData.base64Image;
-                    const contentType = imageData.contentType;
-                    const fullImageSrc = 'data:' + contentType + ';base64,' + base64Image;
-                    setImageSrc(fullImageSrc);
-                    setDisplayImage(true)
-                }
-            } catch (error) {
-                console.error("An error occurred: ", error)
-            }
+  return (
+    <>
+      <NavBarDoctor />
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <>
+          <input
+            type="file"
+            ref={fileInputRef}
+            onChange={handleFileInputChange}
+            style={{ display: "none" }}
+            accept=".jpg, .jpeg, .png"
+          />
+          <div className="button-row">
+            <DropdownButton id="dropdown-basic-button" title="Choose image">
+              {imageMeta.map((item, index) => (
+                <Dropdown.Item
+                  key={index}
+                  onClick={() => handleDropdownItemClick(item.imageId)}
+                >
+                  {item.description}
+                </Dropdown.Item>
+              ))}
+            </DropdownButton>
+            <Button
+              id="upload-image-button"
+              title="Upload image"
+              onClick={openFileSelector}
+            >
+              Upload image
+            </Button>
 
-        }
-    };
+            {showImage && isDoctor && (
+              <Button
+                id="edit-image-button"
+                title="Edit image"
+                onClick={handleEditImage}
+              >
+                Edit image
+              </Button>
+            )}
 
-    useEffect(() => {
-        const storedPatient = localStorage.getItem("currentPatient");
-        if (storedPatient) {
-            const patientData: Patient = JSON.parse(storedPatient);
-            setSelectedPatient(patientData);
-        }
-    }, []);
+            {showSaveButton && (
+              <Button
+                id="save-image-button"
+                title="Save image"
+                onClick={handleSaveImage}
+              >
+                Save image
+              </Button>
+            )}
+          </div>
+        </>
+      )}
 
-    useEffect(() => {
-        if (selectedPatient) {
-            const loadImages = async () => {
-                setLoading(true);
-                try {
-                    const metadata = await fetchImageMetadata(selectedPatient.id);
-                    if (metadata) {
-                        console.log("Metadata fetched");
-                        setImagesMetaData(metadata);
-                    } else {
-                        console.log("Couldn't get metadata");
-                    }
-                } catch (error) {
-                    console.error("Error", error);
-                } finally {
-                    setLoading(false);
-                }
-            };
-            loadImages().then(r => r);
-        }
-    }, [selectedPatient]);
-
-    const imageMeta = imagesMetaData?.images ?? []
-
-    return (<>
-        <NavBarDoctor/>
-        {loading ? (<LoadingSpinner/>) : (<>
-                <input
-                    type="file"
-                    ref={fileInputRef}
-                    onChange={handleFileInputChange}
-                    style={{display: 'none'}}
-                    accept=".jpg, .jpeg, .png"
-                />
-                <div className="button-row">
-                    <DropdownButton
-                        id="dropdown-basic-button"
-                        title="Choose image"
-                    >
-                        {imageMeta.map((item, index) => <Dropdown.Item
-                            key={index}
-                            onClick={() => handleDropdownItemClick(item.imageId)}
-                        >
-                            {item.description}
-                        </Dropdown.Item>)}
-                    </DropdownButton>
-                    <Button id="upload-image-button" title="Upload image" onClick={openFileSelector}>Upload
-                        image</Button>
-                </div>
-            </>
-
-        )}
-
-        {displayImage && (<div>
-            <div className="horizontalCenterWithTopMargin">
-                <CanvasComponent imageUrl={imageSrc} canvasRef={canvasRef}></CanvasComponent>
-            </div>
-        </div>)}
-    </>);
+      {showImage && (
+        <div className="container">
+          <CanvasComponent
+            imageUrl={imageSrc}
+            addText={true}
+            hexColor={canvasColor}
+          ></CanvasComponent>
+          <div className="color-picker">
+            <GithubPicker onChangeComplete={handleChangeComplete}></GithubPicker>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default ImagesPage;
