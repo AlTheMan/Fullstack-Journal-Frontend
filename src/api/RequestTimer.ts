20c9acc33acba8af5b34aa2b1b8e1116e97186df
Emil Wennström <emilwennstrom@gmail.com>
2023-12-30 16:21:22 +0100

    removed unnecessary files

--- src/api/RequestTimer.ts
+++ src/api/RequestTimer.ts
@@ -1,13 +1,13 @@
 export const RequestTimer = () =>{
     console.log("In timer")
-    let currentTime = Date.now().toString()
-    let requestTimer = localStorage.getItem("lastRequestTime")
+    const currentTime = Date.now().toString()
+    const requestTimer = localStorage.getItem("lastRequestTime")
     if (requestTimer){
-        let current = Number(currentTime)
-        let lastRequest = Number(requestTimer)
-        let differens = current - lastRequest
-        console.log("Request avaliable in ", 1 - differens)
-        if (differens > 1) {
+        const current = Number(currentTime)
+        const lastRequest = Number(requestTimer)
+        const difference = current - lastRequest
+        console.log("Request available in ", 1 - difference)
+        if (difference > 1) {
             localStorage.setItem("lastRequestTime", currentTime)
             return true
         }
